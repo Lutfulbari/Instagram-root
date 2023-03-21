6de249863221ecaeaaffe19b9ff2aae52f079c08
lutfu <lutfulbarimusa@gmail.com>
2023-03-22 03:27:44 +0600

    Instagram

---
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.instagram;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+import com.example.instagram.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+
+
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+
+
+
+
+}
